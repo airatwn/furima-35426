742578e6a30a83661cca34b6422b72befb6ac468
airatwn <o98.ogawa.yuta@gmail.com>
2021-05-03 15:15:51 +0900

    ユーザー管理機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence:true
+  validates :email,    presence:true, uniqueness:true
+  validates :password, presence:true, format: { [/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i] }
+  validates :first_name, presence:true
+  validates :first_name_kana, presence:true
+  validates :family_name, presence:true
+  validates :family_name_kana, presence:true
+  validates :birthday, presence:true
+end
+
