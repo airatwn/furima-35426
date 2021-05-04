8b321d1fbb09d30d867a9199e6b5dc3ba03695b4
airatwn <o98.ogawa.yuta@gmail.com>
2021-05-04 16:25:50 +0900

    with_optionsでまとめた

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,14 @@ with_options presence: true do
   validates :nickname
   validates :email, uniqueness:true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
   validates :first_name
-  validates :first_name_kana
   validates :family_name
+end
+with_options format: { with: /\A[ァ-ヶー]+\z/ } do
+  validates :first_name_kana
   validates :family_name_kana
+end
   validates :birthday
  end
 end
