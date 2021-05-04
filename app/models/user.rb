035a7605db98edd439766b478231ae6e8d3367fc
airatwn <o98.ogawa.yuta@gmail.com>
2021-05-04 15:28:54 +0900

    with_optionsでまとめてバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence:true
-  validates :email,    presence:true, uniqueness:true
-  validates :password, presence:true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-  validates :first_name, presence:true
-  validates :first_name_kana, presence:true
-  validates :family_name, presence:true
-  validates :family_name_kana, presence:true
-  validates :birthday, presence:true
+  # validates :nickname, presence:true
+  # validates :email,    presence:true, uniqueness:true
+  # validates :password, presence:true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  # validates :first_name, presence:true
+  # validates :first_name_kana, presence:true
+  # validates :family_name, presence:true
+  # validates :family_name_kana, presence:true
+  # validates :birthday, presence:true
 end
 
+with_options presence: true do
+  validates :nickname
+  validates :email, uniqueness:true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :first_name
+  validates :first_name_kana
+  validates :family_name
+  validates :family_name_kana
+  validates :birthday
+end
