5c022404579d8fbcc0b3b1457d03138403dfdd6a
airatwn <o98.ogawa.yuta@gmail.com>
2021-05-06 18:53:37 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,12 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   with_options presence: true do
-  validates :category_id, :price, :condition, :shipping_charge, :prefecture, :days_to_ship, numericality: { other_than: 1 }
+  validates :category_id, :condition, :shipping_charge, :prefecture, :days_to_ship, numericality: { other_than: 1 }
   end
   with_options presence: true do
   validates numericality: { :only_integer true }
+  with_options presence: true do
+  validates :price, numericality: { in: 300..9999999 }
   end
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
