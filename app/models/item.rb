e2f41efc798006842e14bde0ebcd30ca5e795d34
airatwn <o98.ogawa.yuta@gmail.com>
2021-05-06 14:17:31 +0900

    カラム名を修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,5 +4,5 @@ class Item < ApplicationRecord
 
   validates :category, :price, :condition, :shipping_charge, :prefecture, :days_to_ship, numericality: { other_than: 1 }
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category_id, :price, :condition, :shipping_charge, :prefecture, :days_to_ship
+  belongs_to :category_id, :price, :condition_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id
 end
