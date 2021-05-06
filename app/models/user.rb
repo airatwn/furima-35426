a3f76208038518e7ea7f10b88cfe2b56f7387cb5
airatwn <o98.ogawa.yuta@gmail.com>
2021-05-06 16:50:08 +0900

    不要な記述は削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
 
 with_options presence: true do
   validates :nickname
-  validates :email
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
 with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
   validates :first_name
