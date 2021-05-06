1fa565bfbb06f39f8e698e956ebec973ff149626
airatwn <o98.ogawa.yuta@gmail.com>
2021-05-06 14:51:53 +0900

    アソシエーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,11 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :category, :price, :condition, :shipping_charge, :prefecture, :days_to_ship, numericality: { other_than: 1 }
+  validates :category_id, :price, :condition, :shipping_charge, :prefecture, :days_to_ship, numericality: { other_than: 1 }
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category_id, :price, :condition_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :prefecture 
+  belongs_to :days_to_ship
 end
