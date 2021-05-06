7a763073847cbb472161f01dc8662e89233b1a9f
airatwn <o98.ogawa.yuta@gmail.com>
2021-05-06 14:00:25 +0900

    選択が「---」の時は保存できないようにする設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :category, :price, :condition, :shipping_charge, :prefecture, :days_to_ship, numericality: { other_than: 1 }
 end
