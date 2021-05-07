5ff04137099e465ecbef4b92f4a507c25cdbca10
airatwn <o98.ogawa.yuta@gmail.com>
2021-05-07 18:54:55 +0900

    テストの追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -67,6 +67,22 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not a number")
       end
+
+      it '販売価格が10,000,000以上では登録できない' do
+        @item.price = 10_000_000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+      it '販売価格が半角英語だけでは登録できない' do
+        @item.price = 'money'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it '販売価格が半角英数混合では登録できない' do
+        @item.price = 'money1000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
   end
  end
 end
