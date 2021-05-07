a8404f394b67d7f6f71ea65eec423c02c6e23349
airatwn <o98.ogawa.yuta@gmail.com>
2021-05-07 11:39:27 +0900

    エラー解消

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,18 +1,20 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one_attached :image
   with_options presence: true do
   validates :category_id, :condition, :shipping_charge, :prefecture, :days_to_ship, numericality: { other_than: 1 }
   end
   with_options presence: true do
-  validates numericality: { :only_integer true }
+  # validates :カラム名 numericality: { :only_integer true }
   with_options presence: true do
   validates :price, numericality: { in: 300..9999999 }
   end
-  extend ActiveHash::Associations::ActiveRecordExtensions
+ 
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_charge
   belongs_to :prefecture 
   belongs_to :days_to_ship
+end
 end
