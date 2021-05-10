ab61483556694998ed544eef2fd256dde192af81
airatwn <o98.ogawa.yuta@gmail.com>
2021-05-10 16:02:41 +0900

    before_actionで共通の記述をまとめた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-
+  before_action :find_item, only: [:show, :edit, :update]
   def index
     @items = Item.order('created_at DESC')
   end
@@ -19,18 +19,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     if current_user.id != @item.user_id
       redirect_to root_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.save
       redirect_to item_path
@@ -46,4 +43,7 @@ class ItemsController < ApplicationController
                                  :days_to_ship_id, :item_text).merge(user_id: current_user.id)
   end
 
+  def find_item
+    @item = Item.find(params[:id])
+  end
 end
