c3cd8a7be546772afbcb49e1060ba0b8284ff747
airatwn <o98.ogawa.yuta@gmail.com>
2021-05-12 12:20:47 +0900

    attr_accessorを用いて、保存したい複数のテーブルのカラム名すべてを扱えるように記述

---
+++ app/models/shipping_address.rb
@@ -0,0 +1,11 @@
+class ShippingAddress
+  include ActiveModel::Model
+  attr_accesor :portal_code, :prefecture, :address1, :address2, :building_name, :phone_number, :order, :user_id
+  end
+  # ここにバリデーションの処理を書く
+
+  def save
+    # 各テーブルにデータを保存する処理を書く
+  end
+end
+end
