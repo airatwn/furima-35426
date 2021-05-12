6186179de2dc22c1e9a650ec37eb6a2aaeacb36b
airatwn <o98.ogawa.yuta@gmail.com>
2021-05-12 14:14:09 +0900

    Formオブジェクトでshipping_addressモデルとorderモデルのバリデーションをまとめた記述

--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -3,7 +3,16 @@ class OrderShippingAddress < ApplicationRecord
     attr_accesor :portal_code, :prefecture, :address1, :address2, :building_name, :phone_number, :order, :user_id
     end
     # ここにバリデーションの処理を書く
-  
+  with_options presence true do
+    validates :portal_code,  format: { with: /\A\d{3}[-]\d{4}\z/ } #ハイフンあり7桁 xxx-xxxx
+    validates :prefecture,   numericality: { other_than: 1 }
+    validates :address1
+    validates :address2
+    validates :building_name
+    validates :phone_number, format: { with: /\A\d[0-9]{,11}\z/ } #ハイフンなし11桁以内の数字のみ
+    validates :order
+    validates :user_id
+  end
     def save
       # 各テーブルにデータを保存する処理を書く
     end
