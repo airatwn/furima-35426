c706f41ed16e1b15d57ae8f764540476c6c83adf
airatwn <o98.ogawa.yuta@gmail.com>
2021-05-13 20:06:45 +0900

    エラーメッセージを表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,24 @@
 class OrdersController < ApplicationController
   def index
-    @order = OrderShippingAddressecor.new #フォームオブジェクトのインスタンスを生成し、インスタンス変数に代入する
+    @order_shipping_address = OrderShippingAddress.new #フォームオブジェクトのインスタンスを生成し、インスタンス変数に代入する
     @item = Item.find(params[:item_id])
   end
 
   def create
-    @order = OrderShippingAddressecor.new(order_params)
-    if @order.valid?
-       @order.save
+    @item = Item.find(params[:item_id])
+    @order_shipping_address = OrderShippingAddress.new(order_params)
+    if @order_shipping_address.valid?
+       @order_shipping_address.save
        redirect_to root_path
     else
-       render :new
+       render :index
     end
   end
   
   private
 
   def order_params
-    params.require(:order).permit(:portal_code, :prefecture_id, :address1, :address2, :phone_number).merge(:user_id, current_user.id)
+    params.require(:order_shipping_address).permit(:portal_code, :prefecture_id, :address1, :address2, :building_name, :phone_number,).merge(user_id: current_user.id)
   end
   #requireは引数にしていたorderのkeyだけを取得できる。permitaは許可したいパラメータをフィルタしてくれる。
   #mergeは追加したいカラム名 :値
