5ec8df19f8d21bee715d634353935241a0c8a688
airatwn <o98.ogawa.yuta@gmail.com>
2021-05-15 15:10:58 +0900

    テストコードの記述

--- spec/factories/order_shipping_addresses.rb
+++ spec/factories/order_shipping_addresses.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :order_shipping_address do
-    
+    portal_code              { '666-6666' }
+    prefecture_id            { '47'}
+    address1                 { '札幌市'}
+    address2                 { '旭区1-1-1'}
+    building_name            { 'test'}
+    phone_number             { '08011112222'}
+    # token                    { 'tok_aaaaaaaaaa666666'}
   end
 end
--- spec/models/order_shipping_address_spec.rb
+++ spec/models/order_shipping_address_spec.rb
@@ -1,5 +1,71 @@
 require 'rails_helper'
 
 RSpec.describe OrderShippingAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    @order_shipping_address = FactoryBot.build(:order_shipping_address, user_id: user.id, item_id: item.id)
+    sleep 0.1 
+  end
+
+ describe '商品購入' do
+  context '商品が購入できる時' do
+    it '全項目が埋まっている時購入できる' do
+      # binding.pry
+      expect(@order_shipping_address).to be_valid
+    end
+    it '建物名が空でも商品が購入できる' do
+      @order_shipping_address.building_name = ''
+      expect(@order_shipping_address).to be_valid
+    end
+  end
+
+  context '商品が購入できない時' do
+    it 'クレジットカード情報が空の時購入できない' do
+      @order_shipping_address.token = ''
+      @order_shipping_address.valid?
+      expect(@order_shipping_address.errors.full_messages).to include("")
+    end
+    it '郵便番号が空の時購入できない' do
+      @order_shipping_address.portal_code = ''
+      @order_shipping_address.valid?
+      expect(@order_shipping_address.errors.full_messages).to include("Portal code can't be blank", "Portal code is invalid")
+    end
+    it '郵便番号がハイフンが必要であること' do
+      @order_shipping_address.portal_code = 6665555
+      @order_shipping_address.valid?
+      expect(@order_shipping_address.errors.full_messages).to include("Portal code is invalid")
+    end
+    it '都道府県が空の時購入できない' do
+      @order_shipping_address.prefecture_id = ''
+      @order_shipping_address.valid?
+      expect(@order_shipping_address.errors.full_messages).to include("Prefecture can't be blank")
+    end
+    it '市町村が空の時購入できない' do
+      @order_shipping_address.address1 = ''
+      @order_shipping_address.valid?
+      expect(@order_shipping_address.errors.full_messages).to include("Address1 can't be blank")
+    end
+    it '番地が空の時購入できない' do
+      @order_shipping_address.address2 = ''
+      @order_shipping_address.valid?
+      expect(@order_shipping_address.errors.full_messages).to include("Address2 can't be blank")
+    end
+    it '電話番号が空の時購入できない' do
+      @order_shipping_address.phone_number = ''
+      @order_shipping_address.valid?
+      expect(@order_shipping_address.errors.full_messages).to include("Phone number can't be blank", "Phone number is invalid")
+    end
+    it '電話番号は11桁以内であること' do
+      @order_shipping_address.phone_number = '111112222233333'
+      @order_shipping_address.valid?
+      expect(@order_shipping_address.errors.full_messages).to include("Phone number is invalid")
+    end
+    it '電話番号は数値のみであること' do
+      @order_shipping_address.phone_number = '080-12345678'
+      @order_shipping_address.valid?
+      expect(@order_shipping_address.errors.full_messages).to include("Phone number is invalid")
+    end
+  end
+ end
 end
