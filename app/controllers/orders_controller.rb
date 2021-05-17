402f87cc70a62d3f77f51c197b2eab4c99b3217f
airatwn <o98.ogawa.yuta@gmail.com>
2021-05-17 15:35:11 +0900

    不要な記述は削除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -21,7 +21,7 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_shipping_address).permit(:portal_code, :prefecture_id, :address1, :address2, :building_name, :phone_number, :price).merge(
+    params.require(:order_shipping_address).permit(:portal_code, :prefecture_id, :address1, :address2, :building_name, :phone_number ).merge(
       user_id: current_user.id, token: params[:token], item_id: params[:item_id]
     )
   end
