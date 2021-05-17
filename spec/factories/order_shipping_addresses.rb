64e83a8fd5922aa37f7f40ed03b5088d9e1b6477
airatwn <o98.ogawa.yuta@gmail.com>
2021-05-17 15:06:19 +0900

    active_hashのデータは数値なのでシングルクオテーションを削除

--- spec/factories/order_shipping_addresses.rb
+++ spec/factories/order_shipping_addresses.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :order_shipping_address do
     portal_code              { '666-6666' }
-    prefecture_id            { '47' }
+    prefecture_id            { 47 }
     address1                 { '札幌市' }
     address2                 { '旭区1-1-1' }
     building_name            { 'test' }
